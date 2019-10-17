bea48129c5051b375b1f6d97c09c44811dd6f7c3
HeechanYang <yhc944@gmail.com>
2019-10-17 20:56:46 +0900

    [ADD] 2주차 과제 - Level 2. #2

--- assignment/2nd_assignment/routes/api/group.js
+++ assignment/2nd_assignment/routes/api/group.js
@@ -2,23 +2,41 @@ const csvModule = require('../../modules/csv-module');
 const express = require('express');
 const router = express.Router();
 
-router.get(['', '/'], function(req, res, next) {
-  csvModule.csvToJson("members.csv", (json)=>{
-    res.send(json);
+router.get(['', '/'], function (req, res, next) {
+  csvModule.csvToJson("members.csv", (members) => {
+    csvModule.csvToJson("groups.csv", (groups) => {
+      for (var i = 0; i < members.length; i++) {
+          for (var j = 0; j < groups.length; j++) {
+            if (groupIdx === groups[j].groupIdx) {
+              members[i]["group"] = groups[j].name;
+              break;
+            }
+          }
+      }
+      res.send(members);
+    })
   })
 });
 
-router.get('/:groupIdx', function(req, res, next) {
+router.get('/:groupIdx', function (req, res, next) {
   const {groupIdx} = req.params;
-  
-  csvModule.csvToJson("members.csv", (json)=>{
-    var result = [];
-    for(var i=0;i<json.length;i++){
-      if(json[i].groupId === groupIdx){
-        result.push(json[i]);
+
+  csvModule.csvToJson("members.csv", (members) => {
+    csvModule.csvToJson("groups.csv", (groups) => {
+      var result = [];
+      for (var i = 0; i < members.length; i++) {
+        if (members[i].groupIdx === groupIdx) {
+          for (var j = 0; j < groups.length; j++) {
+            if (groupIdx === groups[j].groupIdx) {
+              members[i]["group"] = groups[j].name;
+              break;
+            }
+          }
+          result.push(members[i]);
+        }
       }
-    }
-    res.send(result);
+      res.send(result);
+    })
   })
 });
 
